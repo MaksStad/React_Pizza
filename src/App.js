a56ec75556f0766212bb1fbf8a2e9beccc94d675
Maks Stadnyk <makstadnik1@gmail.com>
2023-01-28 14:20:45 +0200

    React-Pizza-start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
+
+import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://63cd5c94d4d47898e397781a.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => setItems(arr));
+  }, []);
+
+  fetch('https://63cd5c94d4d47898e397781a.mockapi.io/items')
+
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {items.map((obj) => (
+              <PizzaBlock key={obj.id} {...obj} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

5a045167bf6f26b51be1b3d4a94ac30430380634
Maks Stadnyk <makstadnik1@gmail.com>
2023-02-06 10:44:50 +0200

    Sort-and-filter-pizzas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,22 +9,30 @@ import Skeleton from "../components/PizzaBlock/Skeleton";
 export default function Home() {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [categoryId, setCategoryId] = useState(0);
+  const [sortType, setsortType] = useState({ name: 'популярности', sortProperty: 'rating' });
 
   useEffect(() => {
-    fetch('https://63cd5c94d4d47898e397781a.mockapi.io/items')
+    setIsLoading(true)
+    fetch(
+      `https://63cd5c94d4d47898e397781a.mockapi.io/items?${categoryId > 0 ? `category=${categoryId}` : ''
+      }&sortBy=${sortType.sortProperty}&order=asc`
+    )
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType]);
+
+  console.log('categoryId', categoryId, sortType)
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onClickCategory={(i) => setCategoryId(i)} />
+        <Sort value={sortType} onChangeSort={(i) => setsortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
